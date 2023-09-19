32e21fe87f5b44aced971a5a795000a1617bd88a
salifououzerou25@gmail.com <salifououzerou25@gmail.com>
2023-09-19 17:18:06 +0100

    connexion à la base de données

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
-//on essaye d'établir une connexion avec la base de données
+    session_start();   
+    //on essaye d'établir une connexion avec la base de données
     $db=new PDO("mysql:host=localhost;dbname=marche;charset=utf8",'root','');
 ?>
